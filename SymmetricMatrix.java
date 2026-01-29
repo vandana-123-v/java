94da425631e58faf01a2f1c2cdb2f1ff8182286e
vandana-123-v <vandana.vijayakumar2004@gmail.com>
2026-01-29 15:15:29 +0530

    Add files via upload

---
+++ SymmetricMatrix.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class SymmetricMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows (and columns): ");
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        System.out.println("Enter the matrix row by row:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        boolean isSymmetric = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) { 
+                if (matrix[i][j] != matrix[j][i]) {
+                    isSymmetric = false;
+                    break;
+                }
+            }
+            if (!isSymmetric) {
+                break;
+            }
+        }
+
+        if (isSymmetric) {
+            System.out.println("The matrix is symmetric.");
+        } else {
+            System.out.println("The matrix is not symmetric.");
+        }
+
+        sc.close();
+    }
+}
+
